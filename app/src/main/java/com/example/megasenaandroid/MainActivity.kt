d83edef5094a865a01dabeeeed88ca39c38338d5
Gustavo Silva <gustavodev60@gmail.com>
2023-02-08 22:47:40 -0300

    finaliza primeiro app - android express

--- app/src/main/java/com/example/megasenaandroid/MainActivity.kt
+++ app/src/main/java/com/example/megasenaandroid/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.megasenaandroid
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.view.View.OnClickListener
+
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -12,16 +13,25 @@ import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var prefs: SharedPreferences;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
+        prefs = getSharedPreferences("db", Context.MODE_PRIVATE);
+        val result = prefs.getString("result", "");
+
 
         val editText: EditText = findViewById(R.id.edit_number);
         val txtResult: TextView = findViewById(R.id.txt_result);
         val btnGenerate: Button = findViewById(R.id.btn_generate);
         // SEGUNDA OPÇÃO
        // btnGenerate.setOnClickListener(buttonClickListener)
-
+        result?.let {
+            txtResult.text =it;
+        }
+//        if(!result.isNullOrEmpty()){
+//
+//        }
         btnGenerate.setOnClickListener {
             val text = editText.text.toString();
             numberGenerator(text,txtResult);
@@ -30,11 +40,18 @@ class MainActivity : AppCompatActivity() {
 
     }
     private fun numberGenerator(text:String, txtResult: TextView){
-        val qtd = text.toInt();
-    if(text.isEmpty() ||qtd <6 || qtd > 15){
+
+    if(text.isEmpty()) {
         txtResult.text = "";
         Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_SHORT).show();
+        return;
     }
+        val qtd = text.toInt();
+        if(qtd <6 || qtd > 15) {
+            txtResult.text = "";
+            Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         else {
             val numbers = mutableSetOf<Int>()
@@ -46,10 +63,11 @@ class MainActivity : AppCompatActivity() {
                 break;
             }
         }
-        for (x in numbers){
-            Log.i("numeros", "$x");
-        }
+
         txtResult.text = numbers.joinToString("-");
+            val editor = prefs.edit();
+            editor.putString("result", txtResult.text.toString()).apply();
+
         }
 
 
